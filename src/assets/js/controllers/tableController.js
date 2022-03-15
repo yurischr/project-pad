8a090c7f337802b8a985f5062145e417f14ec8e4
Ares Kok <ares.kok@hva.nl>
2022-03-15 10:05:23 +0100

    Table

--- src/assets/js/controllers/tableController.js
+++ src/assets/js/controllers/tableController.js
@@ -14,23 +14,29 @@ window.addEventListener('load', () => {
     const year = document.querySelector("#selectYear");
 
     day.addEventListener("click", function () {
-        day.style.backgroundColor = "#00509E";
-        day.style.color = "white";
+        day.style.background = "#0063c3";
+        day.style.color = "#FFFFFF";
         period.innerHTML = "Dag";
         tableBody.appendChild(clone);
     });
 
     week.addEventListener("click", function () {
+        week.style.background = "#0063c3";
+        week.style.color = "#FFFFFF";
         period.innerHTML = "Week";
         tableBody.appendChild(clone);
     })
 
     month.addEventListener("click", function () {
+        month.style.background = "#0063c3";
+        month.style.color = "#FFFFFF";
         period.innerHTML = "Month";
         tableBody.appendChild(clone);
     })
 
     year.addEventListener("click", function () {
+        year.style.background = "#0063c3";
+        year.style.color = "#FFFFFF";
         period.innerHTML = "Jaar";
         tableBody.appendChild(clone);
     })
