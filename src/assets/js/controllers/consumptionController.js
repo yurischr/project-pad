8c6c935bfd708f0b081ed2cd0358a62ef2e49ca6
Harmohat Khangura <harmohat.khangura@hva.nl>
2022-03-16 16:38:22 +0100

    Added comments and fixing the tabs click logic

--- src/assets/js/controllers/consumptionController.js
+++ src/assets/js/controllers/consumptionController.js
@@ -29,30 +29,27 @@ export class ConsumptionController extends Controller {
         this.#consumptionView = super.loadHtmlIntoCustomElement("html_views/realtimeCards.html"
             , document.querySelector("#realtime-cards"));
 
+        // this.#consumptionView = super.loadHtmlIntoCustomElement("html_views/components/temp-table.html"
+        //     , document.querySelector("#tableSpace"));
         this.#consumptionView = super.loadHtmlIntoCustomElement("html_views/components/temp-table.html"
             , document.querySelector("#tableSpace"));
-
-        // this.#consumptionView = super.loadHtmlIntoCustomElement("html_views/table.html"
-        //     , document.querySelector("#tableSpace"));
-
         await this.#setDatable()
+
+        document.querySelector("#tableSpace").firstElementChild
 
         const anchors = document.querySelectorAll("a.tab-link");
         anchors.forEach(anchor => anchor.addEventListener("click", (event) => this.#handleTableView(event)))
 
-        await this.#fetchDailyData()
     }
 
     async #handleTableView(event) {
-        this.#consumptionView = super.loadHtmlIntoCustomElement("html_views/components/temp-table.html"
-            , document.querySelector("#tableSpace"));
-
         switch (event.target.dataset.table) {
             case "day":
                 // Call the day method here
                 break;
             case "week":
                 await this.#fetchWeeklyData()
+                document.querySelector(".title-tb").innerHTML = "test"
                 break;
             case "month":
                 // Call the month method here
@@ -70,13 +67,12 @@ export class ConsumptionController extends Controller {
 
             let template = document.querySelector("#row-template");
 
-            console.log(template)
             for (let row in data.data) {
                 let clone = template.content.cloneNode(true);
 
                 clone.querySelector(".time").textContent = data.data[row]['week'];
                 clone.querySelector(".data").textContent = data.data[row]['consumption'];
-                document.querySelector(".table-body").appendChild(clone)
+                document.querySelector(".table-body").append(clone)
             }
         } catch (e) {
             console.log("error while fetching the weekly electricity data", e);
