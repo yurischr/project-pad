59b73612b3d031f7bbfa02c698c084d70a41b8e5
Harmohat Khangura <harmohat.khangura@hva.nl>
2022-04-15 14:29:59 +0200

    Changed the logic of loading the html components

--- src/assets/js/controllers/consumptionController.js
+++ src/assets/js/controllers/consumptionController.js
@@ -30,7 +30,28 @@ export class ConsumptionController extends Controller {
 
         //await for when HTML is loaded, never skip this method call in a controller
         this.#consumptionView = await super.loadHtmlIntoContent("html_views/consumption.html")
-        await this.#setupComponents();
+
+        // Loading the header SVG into the DOM element [CLASS: .header-svg]
+        await super.loadHtmlIntoCustomElement("assets/svg/consumption-header-svg.svg"
+            , document.querySelector(".header-svg"));
+
+        // Selecting all the buttons with the class: dashboard-buttons
+        const dashboardBtns = this.#consumptionView.querySelectorAll("button.dashboard-buttons");
+
+        // Adding event listeners to all the buttons
+        dashboardBtns.forEach(button => button.addEventListener("click", async (event) => {
+            dashboardBtns.forEach((button) => {
+                button.classList.remove('active-dashboard-btn');
+            });
+
+            // Avoids the event from adding an active class to the icon
+            if (event.target.nodeName === "BUTTON") {
+                event.target.classList.add('active-dashboard-btn');
+            }
+
+            // Calling the method for handling the dashboard
+            this.#handleDashboard(event);
+        }))
     }
 
     async #setupComponents() {
@@ -50,10 +71,6 @@ export class ConsumptionController extends Controller {
         await super.loadHtmlIntoCustomElement("html_views/components/comparisonChart.html"
             , document.querySelector(".comparison-chart"));
 
-        // Loading the header SVG into the DOM element [CLASS: .header-svg]
-        await super.loadHtmlIntoCustomElement("assets/svg/consumption-header-svg.svg"
-            , document.querySelector(".header-svg"));
-
         // Loading the nav pills into the DOM element [CLASS: .nav-pills-box]
         await super.loadHtmlIntoCustomElement("html_views/components/nav-pills.html"
             , this.#consumptionView.querySelector(".nav-pills-box"));
@@ -61,24 +78,6 @@ export class ConsumptionController extends Controller {
         // Loading the graph into the DOM element [CLASS: .graph]
         await super.loadHtmlIntoCustomElement("html_views/graph.html"
             , document.querySelector(".graph"));
-
-        // Selecting all the buttons with the class: dashboard-buttons
-        const dashboardBtns = this.#consumptionView.querySelectorAll("button.dashboard-buttons");
-
-        // Adding event listeners to all the buttons
-        dashboardBtns.forEach(button => button.addEventListener("click", async (event) => {
-            dashboardBtns.forEach((button) => {
-                button.classList.remove('active-dashboard-btn');
-            });
-
-            // Avoids the event from adding an active class to the icon
-            if (event.target.nodeName === "BUTTON") {
-                event.target.classList.add('active-dashboard-btn');
-            }
-
-            // Calling the method for handling the dashboard
-            this.#handleDashboard(event);
-        }))
     }
 
     /**
