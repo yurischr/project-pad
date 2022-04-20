e2e51c6c0445ee85b26ba043de5bf1bdc3a7c6ba
Kaan Bas <kaan.bas@hva.nl>
2022-04-20 16:57:47 +0200

    Added daily, weekly and monthly data to graph

--- src/assets/js/controllers/electraController.js
+++ src/assets/js/controllers/electraController.js
@@ -26,14 +26,11 @@ export class ElectraController extends Controller {
             try {
                 await this.#fetchPeriodData(await this.#electricityRepository.getDailyData(), "Dag", "day");
                 await this.#comparisonChart(await this.#comparisonChartRepository.getDailyComparisonData());
+                await this.#graphDaily();
             } catch (e) {
                 console.log("error while executing the data", e);
             }
         })();
-
-
-        // Calling the method to show the graph
-        this.#graph();
 
         // Calling the method for the electricity SVG Animation
         this.#svgAnimation();
@@ -85,18 +82,22 @@ export class ElectraController extends Controller {
             case this.#TAB_DAY:
                 await this.#fetchPeriodData(await this.#electricityRepository.getDailyData(), "Dag", "day");
                 this.#comparisonChart(await this.#comparisonChartRepository.getDailyComparisonData());
+                await this.#graphDaily();
                 break;
             case this.#TAB_WEEK:
                 await this.#fetchPeriodData(await this.#electricityRepository.getWeeklyData(), "Week", "week");
                 this.#comparisonChart(await this.#comparisonChartRepository.getWeeklyComparisonData());
+                await this.#graphWeekly();
                 break;
             case this.#TAB_MONTH:
                 await this.#fetchPeriodData(await this.#electricityRepository.getMonthlyData(), "Maand", "maand");
                 this.#comparisonChart(await this.#comparisonChartRepository.getMonthlyComparisonData());
+                await this.#graphMonthly();
                 break;
             case this.#TAB_YEAR:
                 await this.#fetchPeriodData(await this.#electricityRepository.getYearlyData(), "Jaar", "year");
                 this.#comparisonChart(await this.#comparisonChartRepository.getYearlyComparisonData());
+                await this.#graphYearly();
                 break;
             default:
                 return false;
@@ -182,13 +183,100 @@ export class ElectraController extends Controller {
         const doughnutChart = new Chart(comparisonChart, config);
     }
 
-    async #graph() {
+    async #graphDaily() {
+        this.#clearGraph();
         const graph = document.querySelector("#graph");
-        const yearlyData = this.#electricityRepository.getYearlyData();
-        let consumptionYearly = [];
+        const dailyData = this.#electricityRepository.getDailyData();
+        let dailyConsumption = [];
 
-        for (let i = 0; i < 4; i++) {
-            consumptionYearly[i] = await yearlyData.then(function (results) {
+        dailyConsumption[0] = await dailyData.then(function (results) {
+                return results.data[1521].consumption;
+        });
+
+        dailyConsumption[1] = await dailyData.then(function (results) {
+            return results.data[1522].consumption;
+        });
+
+        dailyConsumption[2] = await dailyData.then(function (results) {
+            return results.data[1523].consumption;
+        });
+
+        dailyConsumption[3] = await dailyData.then(function (results) {
+            return results.data[1524].consumption;
+        });
+
+        dailyConsumption[4] = await dailyData.then(function (results) {
+            return results.data[1525].consumption;
+        });
+
+        dailyConsumption[5] = await dailyData.then(function (results) {
+            return results.data[1526].consumption;
+        });
+
+        dailyConsumption[6] = await dailyData.then(function (results) {
+            return results.data[1527].consumption;
+        });
+
+        const myChart = new Chart(graph, {
+            type: 'line',
+            data: {
+                labels: ['02-03-2022', '03-03-2022', '04-03-2022', '05-03-2022', '06-03-2022', '07-03-2022', '08-03-2022'],
+                datasets: [{
+                    label: 'Verbruik (kWh)',
+                    data: [dailyConsumption[0], dailyConsumption[1], dailyConsumption[2], dailyConsumption[3], dailyConsumption[4], dailyConsumption[5], dailyConsumption[6]],
+                    fill: false,
+                    borderColor: '#0063c3',
+                    tension: 0.4,
+                }]
+            }
+        });
+    }
+
+    async #graphWeekly() {
+        this.#clearGraph();
+        const graph = document.querySelector("#graph");
+        const weeklyData = this.#electricityRepository.getWeeklyData();
+        let weeklyConsumption = [];
+
+        weeklyConsumption[0] = await weeklyData.then(function (results) {
+            return results.data[215].consumption;
+        });
+
+        weeklyConsumption[1] = await weeklyData.then(function (results) {
+            return results.data[216].consumption;
+        });
+
+        weeklyConsumption[2] = await weeklyData.then(function (results) {
+            return results.data[217].consumption;
+        });
+
+        weeklyConsumption[3] = await weeklyData.then(function (results) {
+            return results.data[218].consumption;
+        });
+
+        const myChart = new Chart(graph, {
+            type: 'line',
+            data: {
+                labels: ['2022 - Week 7', '2022 - Week 8', '2022 - Week 9', '2022 - Week 10'],
+                datasets: [{
+                    label: 'Verbruik (kWh)',
+                    data: [weeklyConsumption[0], weeklyConsumption[1], weeklyConsumption[2], weeklyConsumption[3]],
+                    fill: false,
+                    borderColor: '#0063c3',
+                    tension: 0.4,
+                }]
+            }
+        });
+    }
+
+    async #graphMonthly() {
+        this.#clearGraph();
+        const graph = document.querySelector("#graph");
+        const monthlyData = this.#electricityRepository.getMonthlyData();
+        let monthlyConsumption = [];
+
+        for (let i = 0; i < 12; i++) {
+            monthlyConsumption[i] = await monthlyData.then(function (results) {
                 return results.data[i].consumption;
             });
         }
@@ -196,15 +284,48 @@ export class ElectraController extends Controller {
         const myChart = new Chart(graph, {
             type: 'line',
             data: {
-                labels: ['2018', '2019', '2020', '2021'],
+                labels: ['January 2021', 'February 2021', 'March 2021', 'April 2021', 'May 2021', 'June 2021', 'July 2021', 'August 2021', 'September 2021', 'October 2021', 'November 2021', 'December 2021'],
                 datasets: [{
                     label: 'Verbruik (kWh)',
-                    data: [consumptionYearly[0], consumptionYearly[1], consumptionYearly[2], consumptionYearly[3]],
+                    data: [monthlyConsumption[0], monthlyConsumption[1], monthlyConsumption[2], monthlyConsumption[3], monthlyConsumption[4], monthlyConsumption[5], monthlyConsumption[6], monthlyConsumption[7], monthlyConsumption[8], monthlyConsumption[9], monthlyConsumption[10], monthlyConsumption[11]],
                     fill: false,
                     borderColor: '#0063c3',
                     tension: 0.4,
                 }]
             }
         });
+    }
+
+    async #graphYearly() {
+        this.#clearGraph();
+        const graph = document.querySelector("#graph");
+        const yearlyData = this.#electricityRepository.getYearlyData();
+        let yearlyConsumption = [];
+
+        for (let i = 0; i < 4; i++) {
+            yearlyConsumption[i] = await yearlyData.then(function (results) {
+                return results.data[i].consumption;
+            });
+        }
+
+        const myChart = new Chart(graph, {
+            type: 'line',
+            data: {
+                labels: ['2015', '2019', '2020', '2021'],
+                datasets: [{
+                    label: 'Verbruik (kWh)',
+                    data: [yearlyConsumption[0], yearlyConsumption[1], yearlyConsumption[2], yearlyConsumption[3]],
+                    fill: false,
+                    borderColor: '#0063c3',
+                    tension: 0.4,
+                }]
+            }
+        });
+    }
+
+    #clearGraph() {
+        this.#view.querySelector('#graph').remove();
+        this.#view.querySelector('.graph').insertAdjacentHTML("beforeend", '<canvas id="graph">' +
+            '</canvas>');
     }
 }
